ea0acbfb77a9a1f235e8f5f27e7d3e840d7328ea
Germán Stábile <german.stabile@rootstrap.com>
2018-07-20 12:57:05 -0300

    correct indentation

--- src/components/common/Input/index.js
+++ src/components/common/Input/index.js
@@ -19,11 +19,12 @@ const Input = ({
       {label && <Text>{label}</Text>}
     </View>
     <View style={styles.inputErrorLabelContainer}>
-      <View style={[
-        styles.inputContainer,
-        containerStyle,
-        touched && error ? styles.errorInputContainer : {}
-      ]}
+      <View
+        style={[
+          styles.inputContainer,
+          containerStyle,
+          touched && error ? styles.errorInputContainer : {}
+        ]}
       >
         <TextInput
           style={[styles.input, unit && touched && !error ? inputStyle : {}]}
